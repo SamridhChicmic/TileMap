c71d69ce105892c8cb3ecba835457afec763fa5d
SamridhChicmic <samridh.gupta@chicmic.co.in>
2024-03-27 19:04:51 +0530

    initial Commit

---
+++ floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="floor" tilewidth="55" tileheight="32" tilecount="1" columns="1">
+ <image source="TileBase/floor.png" width="55" height="32"/>
+</tileset>
