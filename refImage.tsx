93e5a758f77265aacbcaa1c752a0772f3c549f96
SamridhChicmic <samridh.gupta@chicmic.co.in>
2024-04-03 17:40:51 +0530

    Updated Asset

---
+++ refImage.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="refImage" tilewidth="6683" tileheight="4127" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="6683" height="4127" source="MapBase/MapRef.png"/>
+ </tile>
+</tileset>
